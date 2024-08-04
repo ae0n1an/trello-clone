7f15a5419979b23dd38260af9f5533f5dcf6a78e
Max Verhoef <mver0017@student.monash.edu>
2024-08-04 15:12:20 +1000

    updated clerk middleware to redirect to select-org path

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,33 @@
-import { clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/"])
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-    if (isPublicRoute(req)) return // if it's a public route, do nothing
-    auth().protect() // for any other route, require auth
+    if (isPublicRoute(req)) return; // if it's a public route, do nothing
 
-    if (auth().orgId)
-    afterAuth()
-})
+    const { pathname } = new URL(req.url);
+
+    // if the user is not logged in navigate to sign in page
+    if (!auth().userId) {
+        const signInUrl = new URL('/sign-in', req.url);
+        signInUrl.searchParams.set('returnBack', req.url);
+        return NextResponse.redirect(signInUrl);
+    }
+
+    let path = "/select-org";
+
+    // if the user is logged in and has an organization selected navigate that page
+    if (auth().orgId) {
+        path = `/organization/${auth().orgId}`;
+    }
+
+    // if the user is logged in but has no org selected navigate to /select-org page
+    if (pathname !== path) {
+        const orgSelection = new URL(path, req.url);
+        return NextResponse.redirect(orgSelection);
+    }
+});
 
 export const config = {
     matcher: [
@@ -18,3 +37,4 @@ export const config = {
         '/(api|trpc)(.*)',
     ],
 };
+
