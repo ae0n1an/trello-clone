9a503584a80aafc87fd5023311b4e9f6b4a9f5ed
ae0n1an <max.verhoef0@gmail.com>
2024-08-12 20:10:11 +1000

    feat: added toast for board creation

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,6 +1,7 @@
 import {
     ClerkProvider
 } from '@clerk/nextjs'
+import {Toaster} from "sonner";
 
 export default function PlatformLayout({
    children,
@@ -9,6 +10,7 @@ export default function PlatformLayout({
 }) {
     return (
         <ClerkProvider afterSignOutUrl='/'>
+            <Toaster />
             {children}
         </ClerkProvider>
 )
